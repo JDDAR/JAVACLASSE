8ef7ad5fc6e774bc4b3d8da290dc0858d85a78fd
JDAAR <jdanaconar@gmail.com>
2024-08-20 20:07:11 -0500

    CRUD Categories : agregando categoria nueva; descripcion: primero cree el servicio luego agrege el endPoint en el controlador

--- api-v2-2826502/src/main/java/org/api/java/api_v2_2826502/controllers/CategoryController.java
+++ api-v2-2826502/src/main/java/org/api/java/api_v2_2826502/controllers/CategoryController.java
@@ -3,24 +3,57 @@ package org.api.java.api_v2_2826502.controllers;
 import org.api.java.api_v2_2826502.entities.Category;
 import org.api.java.api_v2_2826502.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
 
-  // Inyectando el servicio
+  // Inyectando el servicio ******************
   @Autowired
   private CategoryService categorieService;
 
-  // EndPoint - Listar TODAS LAS CATEGORIAS
+  // EndPoint - Listar TODAS LAS CATEGORIAS *********************
   @GetMapping("/categories")
   public ResponseEntity<List<Category>> getCategories() {
     return ResponseEntity.ok().body(categorieService.findAll());
   }
+
+  // EndPoint - LISTAR UNA CATEGORIIA POR ID *********************
+  @GetMapping("/categories/{id}")
+  public ResponseEntity<?> getCategory(@PathVariable Long id) {
+    Optional<Category> optCategory = categorieService.findById(id);
+    if (optCategory.isPresent()) {
+      return ResponseEntity.status(HttpStatus.OK).body(categorieService.findById(id));
+    } else {
+      return ResponseEntity.badRequest().body("Categoria con id : " + id + " No encontrada ...");
+    }
+  }
+
+  // Endpoint - CREAR NUEVA CATEGORIA ******************************
+  @PostMapping("/categories")
+  public ResponseEntity<?> crearCategory(@Valid @RequestBody Category category, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      List<String> erroresValidation = new ArrayList<>();
+      bindingResult.getFieldErrors().forEach(error -> {
+        erroresValidation.add(error.getDefaultMessage());
+      });
+      return ResponseEntity.badRequest().body(erroresValidation);
+    }
+    try {
+      return ResponseEntity.status(HttpStatus.CREATED).body(categorieService.save(category));
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body("Error Creando El producto ");
+    }
+  }
+
 }
--- api-v2-2826502/src/main/java/org/api/java/api_v2_2826502/services/CategoryService.java
+++ api-v2-2826502/src/main/java/org/api/java/api_v2_2826502/services/CategoryService.java
@@ -1,11 +1,14 @@
 package org.api.java.api_v2_2826502.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.api.java.api_v2_2826502.entities.Category;
 import org.api.java.api_v2_2826502.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class CategoryService {
@@ -13,8 +16,20 @@ public class CategoryService {
   @Autowired
   private CategoryRepository repository;
 
-  // obtener la lista de Categoryas
+  // obtener la lista de Categoryas *************************************
   public List<Category> findAll() {
     return (List<Category>) repository.findAll();
   }
+
+  // Seleccionar categoria por ID ***************************************
+  public Optional<Category> findById(Long id) {
+    return repository.findById(id);
+  }
+
+  // insertar nueva categoria **************************************
+  @Transactional
+  public Category save(Category category) {
+    return repository.save(category);
+  }
+
 }
